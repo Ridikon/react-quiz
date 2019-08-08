27000fbd865b36a8d70b7f47d29c2e6774d00849
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-08 10:41:08 +0000

    Deploy Ridikon/react-quiz to github.com/Ridikon/react-quiz.git:gh-pages

---
+++ precache-manifest.2cefe02e4c1d67f0826014d27b69c5cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b194fdda0c5d15a404379c9442d83d0",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "be1bf22076c713857113",
+    "url": "/react-quiz/static/css/main.a14d2e22.chunk.css"
+  },
+  {
+    "revision": "bc702601ad421bf40bc1",
+    "url": "/react-quiz/static/js/2.314c6e84.chunk.js"
+  },
+  {
+    "revision": "be1bf22076c713857113",
+    "url": "/react-quiz/static/js/main.282674d9.chunk.js"
+  },
+  {
+    "revision": "260e1d56f90ee7d17fb2",
+    "url": "/react-quiz/static/js/runtime~main.009b0ca2.js"
+  }
+]);
